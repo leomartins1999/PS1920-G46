a851191c796c5de72afa1608ecb0580da82f8a83
GAllen98 <allen98.gp@gmail.com>
2020-04-14 19:08:11 +0100

    Unfollowed.

--- API/model/params/FollowParams.js
+++ API/model/params/FollowParams.js
@@ -9,14 +9,21 @@ class FollowParams{
     constructor(req, volunteer) {
         this.id = req.user.id;
         this.user_type = req.user.user_type;
-        this.followedId = volunteer? req.params.volunteer_id: req.params.org_id
+        if (volunteer){
+            this.followed_id = req.params.volunteer_id;
+            this.followed_type = 'volunteer';
+        }
+        else{
+            this.followed_id = req.params.org_id;
+            this.followed_type = 'org';
+        }
     }
 
     /**
      * validates the consistency of the object
      */
     validate(){
-        return this.id && this.user_type && this.followedId
+        return this.id && this.user_type && this.followed_id
     }
 
 }
