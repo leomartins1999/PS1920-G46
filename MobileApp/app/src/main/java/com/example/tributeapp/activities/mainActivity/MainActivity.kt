24de0601b303be778dcb91118c591abcb8621da2
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2020-04-28 18:26:41 +0100

    Relatorio revised, mobile app stuff

--- MobileApp/app/src/main/java/com/example/tributeapp/activities/mainActivity/MainActivity.kt
+++ MobileApp/app/src/main/java/com/example/tributeapp/activities/mainActivity/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.example.tributeapp
+package com.example.tributeapp.activities.mainActivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tributeapp.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.topAppBar))
     }
 }
