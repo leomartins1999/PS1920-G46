ea48bbdced760a6f06b5e3e1528ee483d843f0c5
GAllen98 <allen98.gp@gmail.com>
2020-05-07 18:24:52 +0100

    volunteer activity changes

--- MobileApp/app/src/main/java/com/example/tributeapp/activities/VolunteerActivity.kt
+++ MobileApp/app/src/main/java/com/example/tributeapp/activities/VolunteerActivity.kt
@@ -2,8 +2,10 @@ package com.example.tributeapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.tributeapp.R
 import com.example.tributeapp.model.Volunteer
+import kotlinx.android.synthetic.main.activity_volunteer.*
 import org.json.JSONObject
 
 class VolunteerActivity : AppCompatActivity() {
@@ -12,9 +14,21 @@ class VolunteerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_volunteer)
 
-        val obj = JSONObject("{\"_id\":\"5e960b0725a53a0e787804ef\",\"name\":\"monkaW\",\"description\":\"description\",\"linkedInLink\":\"www.linkedin.com\",\"imageLink\":\"link to image\",\"following\":{\"5e97280520fef920f4120a12\":\"volunteer\",\"5e960b1925a53a0e787804f0\":\"org\",\"5e960b0725a53a0e787804ef\":\"volunteer\"},\"followers\":{\"5e960b0725a53a0e787804ef\":\"volunteer\",\"5e972ca23b8a6918d84c9183\":\"org\"}}")
+        // TODO: get volunteer from intent
+        val obj = JSONObject("{\"_id\":\"5e960b0725a53a0e787804ef\",\"name\":\"monkaW\",\"description\":\"twitch emote with the funny frog\",\"linkedInLink\":\"www.linkedin.com\",\"imageLink\":\"link to image\",\"following\":{\"5e97280520fef920f4120a12\":\"volunteer\",\"5e960b1925a53a0e787804f0\":\"org\",\"5e960b0725a53a0e787804ef\":\"volunteer\"},\"followers\":{\"5e960b0725a53a0e787804ef\":\"volunteer\",\"5e972ca23b8a6918d84c9183\":\"org\"}}")
         val volunteer = Volunteer(obj)
 
+        updateFields(volunteer)
+
+
         println(volunteer)
+    }
+
+    private fun updateFields(volunteer: Volunteer) {
+        name.text = volunteer.name
+        linkedin.text = volunteer.linkedInLink
+        description.text = volunteer.description
+        followingCount.text = 5.toString()
+        followersCount.text = 2.toString()
     }
 }
