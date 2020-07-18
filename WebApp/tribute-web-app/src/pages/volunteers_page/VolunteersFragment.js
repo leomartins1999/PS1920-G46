07d16e4f852a35f097aeaa6d5412d00fda3c2341
GAllen98 <allen98.gp@gmail.com>
2020-07-18 19:29:41 +0100

    lul

--- WebApp/tribute-web-app/src/pages/volunteers_page/VolunteersFragment.js
+++ WebApp/tribute-web-app/src/pages/volunteers_page/VolunteersFragment.js
@@ -1,17 +1,22 @@
 import React from "react";
 import renderEntity from "./VolunteerCard";
+import {SearchIcon} from "@primer/octicons-react";
+import ClickableIcon from "../../components/ClickableIcon";
+import Loading from "../../components/Loading";
 
 class VolunteersFragment extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            volunteers: []
+            volunteers: [],
+            search: "",
+            newSearch: ""
         }
     }
 
     fetchVolunteers() {
-        return this.props.service.getVolunteers()
+        return this.props.service.getVolunteers(this.state.search)
             .then((volunteers) => {
                 console.log(volunteers)
 
@@ -31,15 +36,30 @@ class VolunteersFragment extends React.Component {
             clearInterval(this.timerID)
     }
 
+    onChangeNewSearch = (e) => {
+        this.setState({newSearch: e.target.value})
+    }
+
+    submitSearch = () => {
+        this.setState({volunteers: [], search: this.state.newSearch})
+        this.fetchVolunteers()
+    }
+
     render() {
         const volunteers = this.state.volunteers
 
-        let volunteersRender = volunteers.map(renderEntity)
+        let volunteersRender = volunteers.length ? volunteers.map(renderEntity) : <Loading/>
 
         return (
-            <div className="jumbotron border-primary m-5">
-                <div className="text-center h1">Volunteers</div>
-                <div className="row justify-content-md-center">{volunteersRender}</div>
+            <div className="card border-primary m-5">
+                <div className="card-header text-center h1">Volunteers</div>
+                <div className="card-body text-center">
+                    <div className="d-inline-flex align-items-center">
+                        <input className="form-control mx-2" placeholder="Search..." value={this.state.newSearch} onChange={this.onChangeNewSearch}/>
+                        <button className="btn btn-link" onClick={this.submitSearch}><SearchIcon size={24}/> </button>
+                    </div>
+                    <div className="row justify-content-md-center">{volunteersRender}</div>
+                </div>
             </div>
         )
     }
