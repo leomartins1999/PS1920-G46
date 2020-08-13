01ab4ad72c47e8d6ff4b4299e07977fe7bc10c61
Leonardo Martins <asmartins.leonardo1999@gmail.com>
2020-08-13 21:49:56 +0100

    small changes

--- MobileApp/app/src/main/java/com/example/tributeapp/api/request_executor/UploadImageRequest.kt
+++ MobileApp/app/src/main/java/com/example/tributeapp/api/request_executor/UploadImageRequest.kt
@@ -3,25 +3,47 @@ package com.example.tributeapp.api.request_executor
 import android.os.AsyncTask
 import android.util.Log
 import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.bumptech.glide.load.model.stream.HttpUriLoader
 import com.example.tributeapp.APP_TAG
+import com.example.tributeapp.App
+import java.net.HttpURLConnection
 import java.net.URL
 
 const val FILE_KEY = "file"
 
 class UploadImageRequest(
     private val image: ByteArray,
-    private val url: String,
+    url: String,
     onSuccess: Response.Listener<String>,
     onError: Response.ErrorListener
-) : AsyncTask<Unit, Unit, String>() {
+) : StringRequest(Method.POST, url, onSuccess, onError) {
 
-    override fun doInBackground(vararg params: Unit?): String {
-        val url = URL(url)
-        val connection = url.openConnection()
-        TODO()
+    override fun getParams(): MutableMap<String, String> {
+        val map = mutableMapOf<String, String>()
+
+        map[FILE_KEY] = String(image)
+
+        return map
     }
 
-    override fun onPostExecute(result: String?) {
-        Log.v(APP_TAG, result!!)
+    override fun getBodyContentType(): String {
+        return "multipart/form-data"
+    }
+
+    override fun getBody(): ByteArray {
+        Log.v(APP_TAG, String(super.getBody()))
+        return super.getBody()
+    }
+
+    override fun getHeaders(): MutableMap<String, String> {
+        // retrieve existing cookies
+        val map = super.getHeaders().toMutableMap()
+
+        // add session cookie
+        map["Cookie"] = App.session!!.user.token
+
+        // return cookies
+        return map
     }
 }
