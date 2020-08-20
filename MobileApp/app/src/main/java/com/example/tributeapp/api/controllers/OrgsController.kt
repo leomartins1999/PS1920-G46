6e1cb3024bb69ea5fe9f44055d7fd14fe2eecb2b
Gallen98 <allen98.gp@gmail.com>
2020-08-20 19:51:30 +0100

    Can now search for volunteers and orgs

--- MobileApp/app/src/main/java/com/example/tributeapp/api/controllers/OrgsController.kt
+++ MobileApp/app/src/main/java/com/example/tributeapp/api/controllers/OrgsController.kt
@@ -1,17 +1,17 @@
 package com.example.tributeapp.api.controllers
 
-import com.example.tributeapp.api.ORGS_URL
 import com.example.tributeapp.api.followOrgURL
 import com.example.tributeapp.api.request_executor.RequestExecutor
 import com.example.tributeapp.api.orgURL
+import com.example.tributeapp.api.orgsURL
 import com.example.tributeapp.api.parser.ListParser
 import com.example.tributeapp.api.parser.SingletonParser
 import com.example.tributeapp.model.dtos.Org
 
 class OrgsController(private val executor: RequestExecutor) {
 
-    fun getOrgs(onSuccess: (List<Org>) -> Unit, onError: () -> Unit) =
-        executor.get(ORGS_URL, ListParser(onSuccess) { Org(it) }, onError)
+    fun getOrgs(searchQuery: String, onSuccess: (List<Org>) -> Unit, onError: () -> Unit) =
+        executor.get(orgsURL(searchQuery), ListParser(onSuccess) { Org(it) }, onError)
 
     fun getOrg(id: String, onSuccess: (Org) -> Unit, onError: () -> Unit) =
         executor.get(orgURL(id), SingletonParser(onSuccess) { Org(it) }, onError)
